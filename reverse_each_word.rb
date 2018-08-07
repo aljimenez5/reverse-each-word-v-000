823b75faaacc5d130fece8f4c9820b5e3d46d590
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-07 02:36:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 def reverse_each_word(string)
   split_string = string.split(" ")
   reverse_sentence = []
-  split_string.each { |word_in_array| reverse_sentence << word_in_array.reverse }
+  split_string.collect { |word_in_array| reverse_sentence << word_in_array.reverse }
   reverse_sentence.join(" ")
 end
